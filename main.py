fced826120527f77d559bd364228ade2c8d23a18
shekofaamiri <shekofaamiri1@gmail.com>
2025-09-29 22:52:26 +0330

    Add elif 2.

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ if len(ch) != 1:
     print("please enter a character:")
 elif '0' <= ch <= '9':
     print("your char is number.")
+elif 'A' <= ch <= 'Z' or 'a' <= ch <= 'z':
+    print("your char is letter.")
